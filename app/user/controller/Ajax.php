879235b9303a2cf186212757a0cb14cb4146696d
AMEN <amen@ymwl.edu.kg>
2023-09-30 14:32:15 +0800

    update dev

--- app/user/controller/Ajax.php
+++ app/user/controller/Ajax.php
@@ -244,14 +244,7 @@ class Ajax
                 try{
                     $decodedData = $decoder->decode($savename);
                 }catch (\Exception $e){
-                    // 默认java位置无java，转变yumjava
-                    $decoder->setJavaPath('/usr/lib/jvm');
-                    try {
-                        $decodedData = $decoder->decode($savename);
-                    } catch (\Exception $e) {
-                        return json(['code'=>900,'msg'=>"网站未配置正确，请咨询站长进行配置，当前错误代码：900".$e->getMessage()]);
-                    }
-//                    return json(['code'=>900,'msg'=>"网站未配置正确，请咨询站长进行配置，当前错误代码：900".$e->getMessage()]);
+                    return json(['code'=>900,'msg'=>"网站未配置正确，请咨询站长进行配置，当前错误代码：900".$e->getMessage()]);
                 }
                 // 获取二维码内容
                 if(!$decodedData->isFound()) {
